0f59f094b2a0fc8dfe3c3a0b31e712672da1acf9
Samuli Saarinen <samuli@crasman.fi>
2020-08-13 12:43:53 +0300

    Add build & deploy scripts

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ morgan.token('post-data', (req, res) => {
 app.use(express.json());
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post-data'));
 app.use(cors());
+app.use(express.static('build'))
 
 let phonebook = [{
 		"name": "Arto Hellas",
