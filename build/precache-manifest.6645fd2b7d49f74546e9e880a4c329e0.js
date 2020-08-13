d68a59db4f01dcefa5599500a87bef2e7a38ae2c
Samuli Saarinen <samuli@crasman.fi>
2020-08-13 12:44:09 +0300

    build-ui

---
+++ build/precache-manifest.6645fd2b7d49f74546e9e880a4c329e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8168c4b674485401fc8db5d6b076907a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db977e8610798d8a546c",
+    "url": "/static/js/2.a64c161a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a64c161a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0a392923d8d10898c10",
+    "url": "/static/js/main.012d6c5a.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
